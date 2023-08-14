f24d567d8b90552a40e1f29b792e86f70f7cdd9e
bcm-ray_crazy <65066644+bcmRayCrazy-coder@users.noreply.github.com>
2023-08-14 18:06:54 +0800

    [fix] cli cannot work

--- dist/socket/registerEvents.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fetchDeclaretion_1 = require("./events/fetchDeclaretion");
-function registerEvents(socket) {
-    socket.on('fetchDeclaretion', fetchDeclaretion_1.fetchDeclaretion);
-}
-exports.default = registerEvents;
---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import typescript from 'typescript'
+import { terser } from 'rollup-plugin-terser'
+import typescript2 from 'rollup-plugin-typescript2'
+import { readFileSync } from 'fs'
+
+const external = Object.keys(JSON.parse((readFileSync('package.json').toString())) || '')
+const globals = external.reduce((prev, current) => {
+  const newPrev = prev
+
+  newPrev[current] = current
+  return newPrev
+}, {})
+
+const defaultConfig = {
+  input: './src/main.ts',
+  output: {
+    file: './dist/main.js',
+    format: 'cjs',
+    banner: '#!/usr/bin/env node',
+    globals
+  },
+  external,
+  plugins: [
+    typescript2({
+      exclude: 'node_modules/**',
+      useTsconfigDeclarationDir: true,
+      typescript,
+      tsconfig: './tsconfig.json'
+    }),
+    terser()
+  ]
+}
+
+export default defaultConfig
